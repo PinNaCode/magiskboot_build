ed6fd86d335876196e525ef6aa5ba316f2295fe9
svoboda18 <dev.svoboda18@gmail.com>
2023-08-04 22:47:01 +0800

    fixup: fix IO modes on MinGW
    * this fixes the line endings in the file outputs
      resolves the data corruption issue
    
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- src/winsup/io_compat.c
+++ src/winsup/io_compat.c
@@ -1,5 +1,8 @@
 #include <sys/types.h>
 #include <io.h>
+#include <stdlib.h>  // for _fmode
+#include <fcntl.h>
+#include <stdio.h>
 
 ssize_t _read_stub(int fd, void *buf, size_t count) {
     return read(fd, buf, count);
@@ -7,4 +10,13 @@ ssize_t _read_stub(int fd, void *buf, size_t count) {
 
 ssize_t _write_stub(int fd, const void *buf, size_t count) {
     return write(fd, buf, count);
+}
+
+// From libnt:
+//      https://github.com/svoboda18/libnt/blob/f3027ff6261a1bfdbcec32de09ac6987aca80f22/libnt.c#L308-L324
+__attribute__((constructor)) static void __init_io_mode(void) {
+    _fmode = O_BINARY;
+    _setmode(_fileno(stdin), O_BINARY);
+    _setmode(_fileno(stdout), O_BINARY);
+    _setmode(_fileno(stderr), O_BINARY);
 }
