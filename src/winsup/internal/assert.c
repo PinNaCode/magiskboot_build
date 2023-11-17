c58a53644c4b964bfdf354d74cefab24931f3a73
Ookiineko <chiisaineko@protonmail.com>
2023-11-17 12:32:45 +0800

    winsup: internal: win_strerror: remove useless mutexes
    * its not thread-safe anyway
    
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- src/winsup/internal/assert.c
+++ src/winsup/internal/assert.c
@@ -3,7 +3,6 @@
 #include <stdarg.h>
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
-#include <pthread.h>
 
 #ifndef NDEBUG
 #define LOG_TAG             "assert_internal"
@@ -30,15 +29,9 @@ void abort_unimpl(const char *s) {
 }
 
 static char __win_strerror_buf[64 * 1024 - 1];
-static pthread_mutex_t __win_strerror_lck;
 static const char *__unknown_strerror = "Unknown error";
 
-__attribute__((constructor)) static void __init_win_strerr_lck(void) {
-    pthread_mutex_init(&__win_strerror_lck, NULL);
-}
-
 const char *win_strerror(DWORD winerr) {
-    pthread_mutex_lock(&__win_strerror_lck);
     if (!FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL,
                        winerr, MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US),
                        __win_strerror_buf, sizeof(__win_strerror_buf), NULL)) {
@@ -47,7 +40,6 @@ const char *win_strerror(DWORD winerr) {
 #endif
         return __unknown_strerror;
     }
-    pthread_mutex_unlock(&__win_strerror_lck);
 
     return __win_strerror_buf;
 }
