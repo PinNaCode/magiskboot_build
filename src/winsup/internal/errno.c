2185373ead9f139781bda1d373b19cd041040f5c
Ookiineko <chiisaineko@protonmail.com>
2023-11-19 22:49:58 +0800

    winsup: internal: errno: warn about unmapped winerr
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- src/winsup/internal/errno.c
+++ src/winsup/internal/errno.c
@@ -3,11 +3,31 @@
 
 #include "winerr_map.h"
 
+#ifndef NDEBUG
+#include "assert.h"
+
+#define LOG_TAG             "errno_internal"
+#define LOG_ERR(...)        log_err(LOG_TAG, __VA_ARGS__);
+#endif
+
 void __set_errno_via_winerr(DWORD winerr) {
-    if (winerr < __winerr_map_size)
-        errno = __winerr_map[winerr];
-    else
+    if (winerr == NO_ERROR)
+        errno = 0;
+    else if (winerr < __winerr_map_size) {
+        int mapped_err = __winerr_map[winerr];
+
+        if (!mapped_err)
+            goto unk_err;
+
+        errno = mapped_err;
+    } else {
+unk_err:
+#ifndef NDEBUG
+        LOG_ERR("Unmapped winerr: %s (%ld)", win_strerror(winerr), winerr)
+#endif
+
         errno = -1;
+    }
 
     return;
 }
