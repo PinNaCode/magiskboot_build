655b6a73ad7c396b66750d22596bded848ed92ea
Ookiineko <chiisaineko@protonmail.com>
2023-12-18 19:35:16 +0800

    fixup: winsup: acl_internal: perform access check on current process instead of user
    * non-elevated admin user doesn't have the privileges
    
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- src/winsup/internal/acl.c
+++ src/winsup/internal/acl.c
@@ -16,6 +16,8 @@
 
 static char userBuff[UNLEN + 1];
 
+static HANDLE hToken = INVALID_HANDLE_VALUE;
+
 int __ensure_path_access(const char *path, DWORD access) {
     char *pathBuff = strdup(path);
 
@@ -38,7 +40,8 @@ int __ensure_path_access(const char *path, DWORD access) {
 
     int ret = -1;
 
-    if ((winerr = GetNamedSecurityInfo(path, SE_FILE_OBJECT, DACL_SECURITY_INFORMATION, NULL, NULL, &oldDacl, NULL, (void **)&sd))) {
+    // owner and group info are required for the AccessCheck below
+    if ((winerr = GetNamedSecurityInfo(path, SE_FILE_OBJECT, OWNER_SECURITY_INFORMATION | GROUP_SECURITY_INFORMATION | DACL_SECURITY_INFORMATION, NULL, NULL, &oldDacl, NULL, (void **)&sd))) {
         SetLastError(winerr);
 
 #ifndef NDEBUG
@@ -52,20 +55,29 @@ int __ensure_path_access(const char *path, DWORD access) {
 
     BuildExplicitAccessWithName(&ea, userBuff, access, GRANT_ACCESS, SUB_CONTAINERS_AND_OBJECTS_INHERIT);
 
-    DWORD old_access = 0;
+    // next, check if our process already have the requested permission
 
-    if ((winerr = GetEffectiveRightsFromAcl(oldDacl, &ea.Trustee, &old_access))) {
-        SetLastError(winerr);
+    GENERIC_MAPPING _mapping = {};  // unused
 
+    // unused
+    PRIVILEGE_SET _privs = {};
+    DWORD _priv_size = sizeof(_privs);
+
+    DWORD _granted = 0;  // unused
+
+    BOOL status = FALSE;
+
+    if (!AccessCheck(sd, hToken, access, &_mapping, &_privs, &_priv_size, &_granted, &status)) {
 #ifndef NDEBUG
-        LOG_ERR("GetNamedSecuirtyInfo failed: %s", win_strerror(winerr))
+        LOG_ERR("AccessCheck failed: %s", win_strerror(GetLastError()))
 #endif
-
         goto exit;
     }
 
-    if (old_access & access)
-        goto success;  // already has the desired access
+    if (status)
+        goto success;  // already has the permission, skip
+
+    // merge with the old DACL
 
     if ((winerr = SetEntriesInAcl(1, &ea, oldDacl, &newDacl))) {
         SetLastError(winerr);
@@ -103,7 +115,7 @@ exit:
     return ret;
 }
 
-__attribute__((constructor)) static void __init_user_name(void) {
+__attribute__((constructor)) static void __init_creds(void) {
     DWORD dw = sizeof(userBuff);
 
     if (!GetUserName(userBuff, &dw)) {
@@ -111,4 +123,29 @@ __attribute__((constructor)) static void __init_user_name(void) {
 
         assert(0);
     }
+
+    // Ref: https://blog.aaronballman.com/2011/08/how-to-check-access-rights/
+
+    HANDLE hProcessToken = INVALID_HANDLE_VALUE;
+
+    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY | TOKEN_DUPLICATE | TOKEN_IMPERSONATE | STANDARD_RIGHTS_READ, &hProcessToken)) {
+        LOG_ERR("OpenProcessToken failed: %s", win_strerror(GetLastError()))
+
+        assert(0);
+    }
+
+    BOOL succeed = DuplicateToken(hProcessToken, SecurityImpersonation, &hToken);
+
+    CloseHandle(hProcessToken);
+
+    if (!succeed) {
+        LOG_ERR("DuplicateToken failed: %s", win_strerror(GetLastError()))
+
+        assert(0);
+    }
+}
+
+__attribute__((destructor)) static void __destroy_creds(void) {
+    if (hToken != INVALID_HANDLE_VALUE)
+        CloseHandle(hToken);
 }
