2164173a09563b9d019201558ee22866210ca25a
Ookiineko <chiisaineko@protonmail.com>
2024-02-12 19:29:55 +0800

    fixup: libc: emscripten: we are not shadowing sendfile()
    - emscripten doesn't provide anything about sendfile
    
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- libc-compat/rs/emscripten/stubs.rs
+++ libc-compat/rs/emscripten/stubs.rs
@@ -8,3 +8,30 @@ extern "C" {
 // syscall
 
 pub const SYS_dup3: crate::c_int = 0;
+
+// sendfile
+
+extern "C" {
+    #[link_name = "__sendfile_stub"]
+    fn real_sendfile(
+        out_fd: crate::c_int,
+        in_fd: crate::c_int,
+        count: crate::size_t
+    );
+}
+
+f! {
+    pub fn sendfile(
+        out_fd: crate::c_int,
+        in_fd: crate::c_int,
+        offset: *mut crate::off_t,
+        count: crate::size_t
+    ) -> crate::ssize_t {
+        if offset.is_null() {
+            real_sendfile(out_fd, in_fd, count);
+            0
+        } else {
+            panic!("unreachable code")
+        }
+    }
+}
