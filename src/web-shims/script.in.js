2ba793d5a7a046f381655d58b581074c5c5ce09f
Ookiineko <chiisaineko@protonmail.com>
2024-04-14 00:51:50 +0900

    web-shims: fix mobile browsers
    - migrates dynamic UI declarations to static HTML
    
    - display correctly on more devices
    
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- src/web-shims/script.in.js
+++ src/web-shims/script.in.js
@@ -2,14 +2,11 @@ if (typeof window !== 'undefined') {
     var Module = {
         'noInitialRun': true,  // prevent calling main() on page load
         'instantiateWasm': (imps, cb) => {
-            var _progress_label = document.createElement('label');
-            var _progress_show = document.createElement('span');
+            var status_label = document.getElementById('status_label').childNodes[0];
+            var status_show = document.getElementById('status_show');
 
-            _progress_label.textContent = 'Fetching: ';
-            _progress_show.textContent = '-- % (N/A)';
-
-            _progress_label.appendChild(_progress_show);
-            document.body.insertBefore(_progress_label, document.body.lastChild);
+            status_label.textContent = 'Fetching: ';
+            status_show.textContent = '-- % (N/A)';
 
             // fetch wasm
 
@@ -20,9 +17,9 @@ if (typeof window !== 'undefined') {
             xhr.onprogress = (ev) => {
                 if (ev.lengthComputable) {
                     const pct = Math.floor(100 * (ev.loaded / ev.total));
-                    _progress_show.textContent = `${pct} % (${ev.loaded} / ${ev.total})`;
+                    status_show.textContent = `${pct} % (${ev.loaded} / ${ev.total})`;
                 } else {
-                    _progress_show.textContent = `-- % (${ev.loaded} / ?)`
+                    status_show.textContent = `-- % (${ev.loaded} / ?)`
                 }
             };
             xhr.onload = async () => {
@@ -30,34 +27,25 @@ if (typeof window !== 'undefined') {
                 var wasm = null;
 
                 if (xhr.status == 200) {
-                    _progress_show.textContent = 'Take longer on slow devices';
-                    _progress_label.textContent = 'Compiling: ';
-                    _progress_label.appendChild(_progress_show);
+                    status_show.textContent = 'Take longer on slow devices';
+                    status_label.textContent = 'Compiling: ';
 
                     try {
                         wasm = await WebAssembly.instantiate(xhr.response, imps);
                         succeed = true;
                     } catch (exc) {
-                        _progress_show.textContent = exc.message;
-                        _progress_label.textContent = 'WebAssembly Error: ';
-                        _progress_label.appendChild(_progress_show);
+                        status_show.textContent = exc.message;
+                        status_label.textContent = 'WebAssembly Error: ';
                     }
                 } else {
-                    _progress_show.textContent = xhr.statusText;
-                    _progress_label.textContent = 'Network Error: ';
-                    _progress_label.appendChild(_progress_show);
+                    status_show.textContent = xhr.statusText;
+                    status_label.textContent = 'Network Error: ';
                 }
 
                 xhr = null;
 
-                if (succeed) {
-                    // free
-                    _progress_label.remove();
-                    _progress_label = undefined;
-                    _progress_show = undefined;
-
+                if (succeed)
                     cb(wasm.instance);
-                }
             };
             xhr.send();
 
@@ -66,67 +54,48 @@ if (typeof window !== 'undefined') {
         'preInit': () => {
             // bind stdout
 
-            const _conout = document.createElement('textarea');
+            const conout = document.getElementById('conout');
             const _dec = new TextDecoder();
-
-            _conout.readOnly = true;
-            _conout.style.fontFamily = 'monospace';
-            _conout.style.width = '1024px';
-            _conout.style.height = '768px';
 
             Module.TTY.stream_ops.write = (_, buff, off, len) => {
                 const arr = buff.subarray(off, off + len);
-                _conout.value += _dec.decode(arr);
-                _conout.scrollTop = _conout.scrollHeight;
+                conout.value += _dec.decode(arr);
+                conout.scrollTop = conout.scrollHeight;
 
                 return len;
             }
-            document.body.insertBefore(_conout, document.body.firstChild);
 
             // exec ctrl
 
-            const _status_label = document.createElement('label');
-            const _status_show = document.createElement('span');
+            const status_label = document.getElementById('status_label').childNodes[0];;
+            const status_show = document.getElementById('status_show');
             window.onerror = (_) => {
-                _status_show.textContent = 'Crashed';
+                status_show.textContent = 'Crashed';
             };
 
-            const _cmdline_edit = document.createElement('input');
-            _cmdline_edit.style.fontFamily = 'monospace';
-            _cmdline_edit.placeholder = 'Arguments…';
+            const cmdline_edit = document.getElementById('cmdline_edit');
+            const clear_btn = document.getElementById('clear_btn');
+            const exec_btn = document.getElementById('exec_btn');
 
-            const _clear_btn = document.createElement('button');
-            const _exec_btn = document.createElement('button');
-
-            _clear_btn.textContent = 'Clear';
-            _clear_btn.addEventListener('click', () => {
-                _status_show.textContent = 'Ready';
-                _conout.value = '';
+            clear_btn.addEventListener('click', () => {
+                status_show.textContent = 'Ready';
+                conout.value = '';
             });
-
-            _exec_btn.disabled = true;
             Module.onRuntimeInitialized = () => {
                 // we can call main() now
-                _status_show.textContent = 'Ready';
-                _status_label.textContent = 'Status: ';
-                _status_label.appendChild(_status_show);
-                _exec_btn.disabled = false;
+                status_show.textContent = 'Ready';
+                status_label.textContent = 'Status: ';
+                exec_btn.disabled = false;
             };
-            _exec_btn.textContent = 'Run';
-            _exec_btn.addEventListener('click', () => {
-                const args = _cmdline_edit.value.split(/\s+/);  // ws seperated args
-                _conout.value += `\n### Arguments: ${_cmdline_edit.value} ###\n\n`;
-                _cmdline_edit.value = '';
+            exec_btn.addEventListener('click', () => {
+                const args = cmdline_edit.value.split(/\s+/);  // ws seperated args
+                conout.value += `\n### Arguments: ${cmdline_edit.value} ###\n\n`;
+                cmdline_edit.value = '';
 
-                _status_show.textContent = 'Running';
+                status_show.textContent = 'Running';
                 const ex = Module.callMain(args);
-                _status_show.textContent = `Exited (code ${ex})`;
+                status_show.textContent = `Exited (code ${ex})`;
             });
-
-            document.body.insertBefore(_cmdline_edit, document.body.firstChild);
-            document.body.insertBefore(_clear_btn, document.body.firstChild);
-            document.body.insertBefore(_exec_btn, document.body.firstChild);
-            document.body.insertBefore(_status_label, _conout.nextSibling);
         },
     };
 }
