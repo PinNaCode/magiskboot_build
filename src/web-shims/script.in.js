3758710d35e071c5cff92e5f72f3daf8c173a84a
Ookiineko <chiisaineko@protonmail.com>
2024-04-14 17:47:42 +0900

    web-shims: clean up
    - XHR exception handling
    
    - remove useless buttons
    
    - fixup some mistakes
    
    - minor reorganizing
    
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- src/web-shims/script.in.js
+++ src/web-shims/script.in.js
@@ -6,11 +6,11 @@ if (typeof window !== 'undefined') {
             var status_show = document.getElementById('status_show');
 
             status_label.textContent = 'Fetching: ';
-            status_show.textContent = '-- % (N/A)';
+            status_show.textContent = '-- % (? / ?)';
 
             // fetch wasm
 
-            var xhr = new XMLHttpRequest();
+            const xhr = new XMLHttpRequest();
 
             xhr.open('GET', '@MAGISKBOOT_WASM_NAME@.wasm', true);
             xhr.responseType = 'arraybuffer';
@@ -18,34 +18,43 @@ if (typeof window !== 'undefined') {
                 if (ev.lengthComputable) {
                     const pct = Math.floor(100 * (ev.loaded / ev.total));
                     status_show.textContent = `${pct} % (${ev.loaded} / ${ev.total})`;
-                } else {
-                    status_show.textContent = `-- % (${ev.loaded} / ?)`
-                }
+                } else
+                    status_show.textContent = `-- % (${ev.loaded} / ?)`;
             };
             xhr.onload = async () => {
                 var succeed = false;
                 var wasm = null;
 
-                if (xhr.status == 200) {
-                    status_show.textContent = 'Take longer on slow devices';
+                if (xhr.status === 200) {
                     status_label.textContent = 'Compiling: ';
+                    status_show.textContent = 'Take longer on slow devices';
 
                     try {
                         wasm = await WebAssembly.instantiate(xhr.response, imps);
                         succeed = true;
                     } catch (exc) {
-                        status_show.textContent = exc.message;
                         status_label.textContent = 'WebAssembly Error: ';
+                        status_show.textContent = exc.message;
                     }
                 } else {
+                    status_label.textContent = 'HTTP Error: ';
                     status_show.textContent = xhr.statusText;
-                    status_label.textContent = 'Network Error: ';
                 }
-
-                xhr = null;
 
                 if (succeed)
                     cb(wasm.instance);
+            };
+            xhr.onerror = (_) => {
+                status_label.textContent = 'Network Error';
+                status_show.textContent = '';
+            };
+            xhr.onabort = (_) => {
+                status_label.textContent = 'Network Error: ';
+                status_show.textContent = 'Connection is aborted';
+            };
+            xhr.ontimeout = (_) => {
+                status_label.textContent = 'Network Error: ';
+                status_show.textContent = 'Request timeout';
             };
             xhr.send();
 
@@ -67,29 +76,24 @@ if (typeof window !== 'undefined') {
 
             // exec ctrl
 
-            const status_label = document.getElementById('status_label').childNodes[0];;
+            const status_label = document.getElementById('status_label').childNodes[0];
             const status_show = document.getElementById('status_show');
             window.onerror = (_) => {
                 status_show.textContent = 'Crashed';
             };
 
             const cmdline_edit = document.getElementById('cmdline_edit');
-            const clear_btn = document.getElementById('clear_btn');
             const exec_btn = document.getElementById('exec_btn');
 
-            clear_btn.addEventListener('click', () => {
-                status_show.textContent = 'Ready';
-                conout.value = '';
-            });
             Module.onRuntimeInitialized = () => {
                 // we can call main() now
-                status_show.textContent = 'Ready';
                 status_label.textContent = 'Status: ';
+                status_show.textContent = 'Ready';
                 exec_btn.disabled = false;
             };
             exec_btn.addEventListener('click', () => {
+                conout.value = '';  // clear old output
                 const args = cmdline_edit.value.split(/\s+/);  // ws seperated args
-                conout.value += `\n### Arguments: ${cmdline_edit.value} ###\n\n`;
                 cmdline_edit.value = '';
 
                 status_show.textContent = 'Running';
